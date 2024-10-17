a458eb0d234324b469c554748811f9443f720e94
percy <125946099+Percylevent@users.noreply.github.com>
2024-10-17 10:10:59 +0800

    Update SIYISDK.py

--- SIYISDK.py
+++ SIYISDK.py
@@ -1,3 +1,16 @@
+#name='SIYISDK',      # 包名
+#version='0.1.0',               # 版本号
+#packages=find_packages(),      # 自动找到项目中的所有包
+#install_requires=['pynput'],     #依赖项
+#author='张鹏程',            # 作者信息
+#author_email='zhangpengcheng@sjtu.edu.cn',  # 作者邮箱
+#description='A python SDK for SIYI A8mini camera',  # 包的描述
+#long_description=open('README.md').read(),    # 详细描述，通常是 README 文件
+#long_description_content_type='text/markdown', # README 文件的格式
+#url='https://github.com/Percylevent/SIYISDK',  # 项目的 URL（GitHub repo 或其他）
+#'Programming Language :: Python :: 3',  # Python 版本
+#'License :: OSI Approved :: MIT License',  # License 类型
+#'Operating System :: OS Independent',
 ﻿
 import socket
 from core import *
